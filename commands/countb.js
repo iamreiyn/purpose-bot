4e86eb3c0c1606b5a0a97ced356fc0ca0b1a1558
renisal <98770205+renisal@users.noreply.github.com>
2022-05-09 00:59:27 +0530

    Improved subreddit cmd and set count(b) limit

--- commands/countb.js
+++ commands/countb.js
@@ -1,5 +1,6 @@
 exports.run = (client, message, args) => {
 	if (!args[0]) return message.reply('Incorrect way! please define a number');
+	if (args[0] > 200) return message.reply('I cannot count any higher than 200');
 	for (let i = args[0]; i >= 0; i--) {
 		message.channel.send('' + i);
 	}
