3a5d4ca098f238467d2cf1a0fe6c73d4cea196b4
Mezo <dominik@koch-bautechnik.de>
2022-05-08 16:40:16 +0200

    Better code!

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 module.exports = (client, message) => {
 	if (message.author.bot) return;
-	if (message.content.indexOf(client.config.prefix) !== 0) return;
+	if (message.content.indexOf(process.env.PREFIX) !== 0) return;
 
-	const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 
 	const cmd = client.commands.get(command);
