482d91ac0bfb1d8a17532f11fdfbf75a7177bb11
renisal <98770205+renisal@users.noreply.github.com>
2022-05-28 16:01:27 +0530

    Improved commands (kick & ban)

--- commands/ban.js
+++ commands/ban.js
@@ -1,23 +1,24 @@
 const { Permissions } = require('discord.js');
 
 exports.run = async (client, message, args) => {
-	if (!args[0]) return message.reply('Invalid format, please define a user ID to ban.');
-	if (message.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS])) {
-		const user = await message.guild.members.fetch(args[0]);
-		if (!args[1]) return args[1] = 'No reason specified.';
-		if (user) {
-			try {
-				user.ban({ reason: args[1] });
+	message.guild.members.fetch(process.env.CLIENTID).then(function(bot) {
+		if (message.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS])) {
+			if (!args[0]) return message.reply('Invalid format, please define a user ID to ban.');
+			message.mentions.members.first() || message.guild.members.fetch(args[0]).then(function(result) {
+				if (!args[1]) return args[1] = 'No reason specified';
+				if (result.roles.highest.position > bot.roles.highest.position) { return message.reply('I cannot ban this member; the specified user\'s role is placed higher than mine.'); }
+				result.ban({ reason: args[1] });
 				message.reply('Successfully banned the user from the discord server.');
-			}
-			catch {
-				message.reply('I do not have permissions to ban');
-			}
+			}).catch(function() {
+				return message.channel.send('Invalid format, could not find the user or user does not exist.');
+			});
 		}
 		else {
-			message.reply('You do not have permissions to ban');
+			message.reply('You do not have permissions to ban this member!');
 		}
-	}
+	}).catch(function(error) {
+		console.log(error);
+	});
 };
 
 exports.name = 'ban';
--- commands/kick.js
+++ commands/kick.js
@@ -1,10 +1,24 @@
+const { Permissions } = require('discord.js');
+
 exports.run = async (client, message, args) => {
-	if (!args[0]) return message.reply('Invalid format, please define a user ID to kick.');
-	if (!args[1]) { args[1] = 'No reason specified';}
-	const user = await message.guild.members.fetch(args[0]);
-	if (!user) return message.reply('Unable to fetch the defined user.');
-	user.kick(args[1]);
-	message.reply('Successfully kicked the user from the server.');
+	message.guild.members.fetch(process.env.CLIENTID).then(function(bot) {
+		if (message.member.permissions.has([Permissions.FLAGS.KICK_MEMBERS])) {
+			if (!args[0]) return message.reply('Invalid format, please define a user ID to kick.');
+			message.mentions.members.first() || message.guild.members.fetch(args[0]).then(function(result) {
+				if (!args[1]) return args[1] = 'No reason specified';
+				if (result.roles.highest.position > bot.roles.highest.position) { return message.reply('I cannot kick this member; the specified user\'s role is placed higher than mine.'); }
+				result.kick(args[1]);
+				message.reply('Successfully kicked the user from the discord server.');
+			}).catch(function() {
+				return message.channel.send('Invalid format, could not find the user or user does not exist.');
+			});
+		}
+		else {
+			message.reply('You do not have permissions to kick this member!');
+		}
+	}).catch(function(error) {
+		console.log(error);
+	});
 };
 
 exports.name = 'kick';
