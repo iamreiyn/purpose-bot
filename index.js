573cefc057cd79ea28b1fa07417d1a3ad9cb16cc
renisal <montymahato@outlook.com>
2023-04-24 22:50:38 +0530

    Update files

--- index.js
+++ index.js
@@ -31,18 +31,40 @@ client.once('ready', () => {
 			'>',
 		)} ${ChalkAdvanced.green('Purpose is now Online')}`,
 	);
-	client.user.setActivity(`with ${process.env.PREFIX}help | purpose.gg`, {
+	client.user.setActivity(`with ${process.env.PREFIX}help | renisal.tech`, {
 		type: 'PLAYING',
 	});
 });
 
-const badWords = ['idiot', 'retard'];
+client.on("InteractionCreate", async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+const badWords = ['idiot', 'stupid'];
 client.on('messageCreate', async (msg) => {
 	// INCOMPLETE WORK
 	badWords.forEach((word) => {
 		if (msg.content.includes(word)) {
 			return msg.reply(
-				'Please avoid using offensive language in this discord server!',
+				'Please avoid using offensive language in our discord server!',
 			);
 		}
 	});
